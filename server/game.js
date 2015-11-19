097681327ad3ac702021187f2142123c3e01f52a
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-19 17:36:55 -0500

    attempted to build state class

--- server/game.js
+++ server/game.js
@@ -3,7 +3,7 @@ var plants = require('./testNewGame')[0];
 var players = require('./testNewGame')[1];
 var resourceRound = require('./game/resourceRound');
 
-var Game = function(players, plants, cities, connections) {
+function Game(players, plants, cities, connections) {
 	this.players = shuffle(players);
 	if(this.players.length < 2 || this.players.length > 6) {
 		throw new Error('not a valid number of players');
@@ -56,7 +56,7 @@ function removePlants(shuffledPlants, numPlayers) {
 	return shuffledPlants;
 }
 
-game = new Game(players, plants, [], []);
+//game = new Game(players, plants, [], []);
 // console.log(game);
 // console.log('player0: ' + game.players[0])
 // console.log('amount in plant deck:' + game.plantDeck.length)
