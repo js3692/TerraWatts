d008f1d91fef85dd529cd0a3ab7cab3ddf75424b
Luis Martins <luis.m.martins91@gmail.com>
2015-11-20 12:51:23 -0500

    Can now leave a game

--- browser/js/grid/grid.js
+++ browser/js/grid/grid.js
@@ -14,12 +14,20 @@ app.config(function ($stateProvider) {
 	})
 })
 
-app.controller('gridCtrl', function ($scope, theGrid, thePlayer, GridFactory) {
+app.controller('gridCtrl', function ($scope, $state, theGrid, thePlayer, GridFactory) {
     console.log('theGrid', theGrid);
 	$scope.grid = theGrid;
+	console.log("$scope.grid", $scope.grid)
 	$scope.me = thePlayer;
+
 	$scope.startGame = function() {
-        
 		GridFactory.start($scope.grid._id)
+	}
+
+	$scope.leaveGame = function() {
+		GridFactory.leaveGame($scope.grid._id)
+		.then(function (grid) {
+			$state.go('home');
+		})
 	}
 })
