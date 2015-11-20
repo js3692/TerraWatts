2b5ae414cf13a02a1c01dcf8148c73a2e22be007
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-19 23:20:46 -0500

    simplified game route

--- server/app/routes/api/gridRouter.js
+++ server/app/routes/api/gridRouter.js
@@ -30,35 +30,34 @@ router.get('/canjoin', function (req, res, next) {
 	.catch(next);
 });
 
+router.param('gridId', function(req, res, next, gridId){
+    Grid.findById(gridId)
+        .then(function(grid){
+            req.grid = grid;
+            next();
+        })
+        .catch(next);
+})
+
 router.get('/:gridId', function (req, res, next) {
-	Grid.findById(req.params.gridId).populate('users')
-	.then(function (grid) {
-		res.sendStatus(200);
-	})
-	.catch(next);
+	if(req.grid) res.sendStatus(200);
 });
 
 router.post('/:gridId/join', function (req, res, next) {
-	Grid.findById(req.params.gridId)
-	.then(function (grid) {
-		return grid.addUser(req.user);
-	})
-	.then(function (grid) {
-		res.json(grid);
-	})
-	.catch(next);
+	req.grid.addUser(req.user)
+	   .then(function (grid) {
+            res.json(grid);
+	   })
+	   .catch(next);
 });
 
-// router.put('/:gridId/start', function(req, res, next) {
-// 	Grid.findById(req.params.gridId)
-// 	.then(function (grid) {
-// 		//user Game constructor here instead of using dummy object
-// 		grid.game = {hey: 'I am a game'};
-// 		return grid.save();
-// 	})
-// 	.then(function (grid) {
-// 		res.json(grid);
-// 	}, next);
-// });
+ router.put('/:gridId/start', function(req, res, next) {
+     
+    req.grid.game = {hey: 'I am a game'};
+    req.grid.save()
+        .then(function (grid) {
+            res.json(grid);
+        }, next);
+ })
 
 module.exports = router;
