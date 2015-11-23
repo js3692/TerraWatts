5e74f3cc2d4f13d98844aa976ef0389da411a1cd
Luis Martins <luis.m.martins91@gmail.com>
2015-11-23 12:45:32 -0500

    Map groundwork is down

--- browser/js/game/game.controller.js
+++ browser/js/game/game.controller.js
@@ -5,4 +5,13 @@ app.controller('GameCtrl', function($scope, grid, $firebaseObject){
     var syncObject = $firebaseObject(ref);
       
     syncObject.$bindTo($scope, "data");
+
+    // MOCK TEST DATA BELOW
+    $scope.gameData = [
+    	{name: 'city1', x: 200, y: 200, slot10: null, slot15: null, slot20: null},
+    	{name: 'city2', x: 400, y: 400, slot10: 'player1', slot15: null, slot20: null},
+    	{name: 'city3', x: 600, y: 600, slot10: 'player2', slot15: 'player1', slot20: null},
+    	{name: 'city4', x: 800, y: 800, slot10: 'player3', slot15: 'player2', slot20: 'player1'}
+    ]
+
 });
