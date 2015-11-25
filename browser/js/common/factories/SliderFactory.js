d5849b583028f8559c57c1585987688cf409cdb7
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-25 18:36:36 -0500

    finished bottom panel

--- browser/js/common/factories/SliderFactory.js
+++ browser/js/common/factories/SliderFactory.js
@@ -1,6 +1,7 @@
 app.factory('SliderFactory', function(){
     return {
         slideOut: function(side){
+            console.log('in slide out')
             var sliders = [].slice.call(document.getElementsByClassName(side + '-slider'));
             sliders.forEach(slider => angular.element(slider).toggleClass(side + '-slider-pull'));
         }
--- browser/js/game/game.controller.js
+++ browser/js/game/game.controller.js
@@ -1,5 +1,4 @@
 app.controller('GameCtrl', function($scope, FirebaseFactory, gridId, key){
     $scope.grid = FirebaseFactory.getConnection(key);
-    $scope.gridGame = FirebaseFactory.getConnection(key + '/game');
-    console.log('scope.grid', $scope.grid) 
+    $scope.gridGame = FirebaseFactory.getConnection(key + '/game'); 
 }); 
