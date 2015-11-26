ce903e3e43957b4326b9160153e8ed7c42f1ffcb
Luis Martins <luis.m.martins91@gmail.com>
2015-11-26 14:19:19 -0500

    About to rebase

--- browser/js/common/directives/game.directive.js
+++ browser/js/common/directives/game.directive.js
@@ -14,8 +14,8 @@ app.directive('game', function($parse) {
 
 		 		if(cities) {
 
-				    var svgWidth = 1200,
-						svgHeight = 800,
+				    var svgWidth = 1440,
+						svgHeight = 900,
 						buffer = 40;
 
 			    	var xBank = [],
@@ -33,11 +33,40 @@ app.directive('game', function($parse) {
 			    		yMax = Math.max.apply(null, yBank),
 			    		yRange = yMax-yMin;
 
-					var map = d3.select('#gameMap')
+					var svg = d3.select('#gameMap')
 						.append('svg')
 						.attr('id', 'map')
 						.attr('width', svgWidth) 
 						.attr('height', svgHeight);
+
+					var width = 960,
+						height = 200;
+
+					d3.json("/utils/maps/us-topo.json", function(error, us) {
+						if(error) return console.error(error);
+						console.log(us);
+
+
+						var projection = d3.geo.albers()
+							.center([0, 55.4])
+							// .center([-98.5795, 39.8282])
+							// .rotate([4.4, 0])
+							.parallels([50, 60])
+							.scale(2500)
+							.translate([width / 2, height / 2]);
+
+
+						var usa = d3.select('#map')
+							.append('path')
+							// .attr('fill', '#cdc')
+							.datum(topojson.feature(us, us.objects.land))
+							.attr('d', d3.geo.path().projection(projection))
+
+
+					});
+
+
+
 
 					var mapCities = d3.select('#map')
 						.append('g')
