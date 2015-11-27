b14744ff17c725f08c455988d21f7ab43a7af2ea
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-27 16:10:23 -0500

    almost all panel details interpolated -- need styling

--- browser/js/common/directives/bottomPanel/bottomPanel.js
+++ browser/js/common/directives/bottomPanel/bottomPanel.js
@@ -7,12 +7,6 @@ app.directive('bottomPanel', function(SliderFactory){
         },
         link: function(scope, elem, attrs){
             scope.open = SliderFactory.slideOut.bind(null, 'bottom');
-            scope.resourceSpecs = {
-                coal: { color: "brown", max: 24 },
-                nuke: { color: "red", max: 12 },
-                oil: { color: "black", max: 24 },
-                trash: { color: "yellow", max: 24 }
-            };
         }
     } 
 });
