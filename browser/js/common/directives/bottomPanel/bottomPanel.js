9e0c06ef3110e037f6624ec2c99be6e7406b4db3
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-27 15:14:29 -0500

    templated out resources and plants

--- browser/js/common/directives/bottomPanel/bottomPanel.js
+++ browser/js/common/directives/bottomPanel/bottomPanel.js
@@ -6,7 +6,13 @@ app.directive('bottomPanel', function(SliderFactory){
            resources: '='
         },
         link: function(scope, elem, attrs){
-            scope.open = SliderFactory.slideOut.bind(null, 'bottom');    
+            scope.open = SliderFactory.slideOut.bind(null, 'bottom');
+            scope.resourceSpecs = {
+                coal: { color: "brown", max: 24 },
+                nuke: { color: "red", max: 12 },
+                oil: { color: "black", max: 24 },
+                trash: { color: "yellow", max: 24 }
+            };
         }
     } 
 });
