77cb5a40521a7f75ee12fc19c0e20bf0d75741ba
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-28 18:55:32 -0500

    updated resource graphs

--- browser/js/common/directives/resourceChart/resourceChart.js
+++ browser/js/common/directives/resourceChart/resourceChart.js
@@ -1,34 +1,41 @@
- app.directive('bars', function ($parse) {
-     return {
-         restrict: 'E',
-         replace: true,
-         template: '<div id="chart"></div>',
-         scope: {
-             data: '='
-         },
-         link: function (scope, element, attrs) {
-             var data = [];
-             scope.$watch('data', function (resources) {
+app.directive('bars', function ($parse) {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div id="chart"></div>',
+        scope: {
+            data: '='
+        },
+        link: function (scope, element, attrs) {
+            var resourceColors = {
+                coal: 'brown',
+                oil: 'black',
+                trash: 'yellow',
+                nuke: 'red'
+            };
 
-                 for (var key in resources) {
-                     data.push(resources[key]);
-                     console.log(resources[key])
-                 }
+            scope.$watch('data', function (resources) {
+                var data = [];
 
-                 var chart = d3.select('#chart')
-                     .append("div").attr("class", "chart")
-                     .selectAll('div')
-                     .data(data).enter()
-                     .append("div")
-                     .transition().ease("elastic")
-                     .style("width", function (d) {
-                         return d + "%";
-                     })
-                     .text(function (d) {
-                         return d + "%";
-                     });
-
-             })
-         }
-     };
- });
+                for (var key in resources) {
+                    data.push({ value: resources[key], type: key, color: resourceColors[key]});
+                }
+                var chart = d3.select('#chart')
+                    .append("div").attr("class", "chart")
+                    .selectAll('div')
+                    .data(data).enter()
+                    .append("div")
+                    .transition().ease("elastic")
+                    .style("width", function (d) {
+                        return d.value + "%";
+                    })
+                    .style("background-color", function(d){
+                        return d.color;
+                    })
+                    .text(function (d) {
+                        return d.type;
+                    });
+            })
+        }
+    };
+});
