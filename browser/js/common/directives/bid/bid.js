bae486b0cde2c38128723259045793ff1c40538e
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-28 15:13:56 -0500

    finished plant auction modal

---
+++ browser/js/common/directives/bid/bid.js
@@ -0,0 +1,19 @@
+app.directive('bid', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/bid/bid.html',
+        scope: {
+            start: '='
+        },
+        link: function (scope, elem, attrs) {
+            scope.bid = scope.start;
+            scope.changeBid = function (quantity) {
+                scope.bid += quantity;
+            }
+
+            scope.isMin = function (bid) {
+                return bid <= scope.start;
+            }
+        }
+    }
+})
--- browser/js/common/directives/plantPanel/plantPanel.js
+++ browser/js/common/directives/plantPanel/plantPanel.js
@@ -1,4 +1,4 @@
-app.directive('plantPanel', function(SliderFactory){
+app.directive('plantPanel', function(SliderFactory, $uibModal){
     return {
         restrict: "E",
         templateUrl: "js/common/directives/plantPanel/plantPanel.html",
@@ -7,7 +7,24 @@ app.directive('plantPanel', function(SliderFactory){
         },
         link: function(scope, elem, attrs){   
             scope.open = SliderFactory.slideOut.bind(null, 'plant');
-          
+            scope.firstFour = function(index){
+                if(index < 4) return 1;
+                return .5;
+            } 
+            
+            scope.bidFor = function (plant) {
+              var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'js/common/directives/bidModal/bidModal.html',
+                    controller: 'BidModalCtrl',
+                    size: 'sm',
+                    resolve: {
+                      plant: function () {
+                        return plant;
+                      }
+                    }
+                });   
+            };
         }
     }
 })
