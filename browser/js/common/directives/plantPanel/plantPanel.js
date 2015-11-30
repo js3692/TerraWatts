2f2907af53baad3fda123d7add0eb695969694b5
Daniel Peach <danielpeach@Daniels-MacBook-Pro-4.local>
2015-11-30 15:30:31 -0500

    updated plant/resource panel

--- browser/js/common/directives/plantPanel/plantPanel.js
+++ browser/js/common/directives/plantPanel/plantPanel.js
@@ -3,14 +3,53 @@ app.directive('plantPanel', function(SliderFactory, $uibModal){
         restrict: "E",
         templateUrl: "js/common/directives/plantPanel/plantPanel.html",
         scope: {
-            plantMarket: '='
+            plantMarket: '=',
+            resources: '='
         },
         link: function(scope, elem, attrs){   
+            scope.plantsTrueResourcesFalse = false;
             scope.open = SliderFactory.slideOut.bind(null, 'plant');
+            scope.toggleArrows = SliderFactory.toggleSliderArrowsHandler();
+            
+            scope.changeView = function(view){
+                var viewObj = {
+                    plants: true,
+                    resources: false
+                };
+                scope.plantsTrueResourcesFalse = viewObj[view];
+            }
+            
+            
+            
             scope.firstFour = function(index){
                 if(index < 4) return 1;
                 return .5;
             } 
+            
+            scope.resourceColors = {
+                coal: '#C8824D',
+                oil: 'black',
+                trash: '#A8A818',
+                nuke: 'red'
+            };
+
+            scope.buyResources = function () {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'js/common/directives/resourceModal/resourceModal.html',
+                    controller: 'ResourceModalCtrl',
+                    size: 'sm',
+                    resolve: {
+                        resources: function () {
+                            return scope.resources;
+                        },
+                        resourceColors: function () {
+                            return scope.resourceColors;
+                        }
+
+                    }
+                });
+            };
             
             scope.bidFor = function (plant) {
               var modalInstance = $uibModal.open({
