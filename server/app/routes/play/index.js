0b2168b0f3b8e8a3c372b4bac75a95d83423fa70
Mike Ingber <mbingber@gmail.com>
2015-12-01 12:29:36 -0500

    structure for continue route

--- server/app/routes/play/index.js
+++ server/app/routes/play/index.js
@@ -16,3 +16,20 @@ router.use('/cityState', require('./cityState'));
 router.use('/endOfTurn', require('./endOfTurn'));
 
 module.exports = router;
+
+router.post('/:gridId...', function(req, res, next) {
+	// find grid
+	var update = req.body;
+	// run validation functions
+		// if any are false, respond with some error, and the game
+		// if all true:
+		grid.state.continue(update, grid.game)
+		.then(function () {
+			return grid.findById(req.grid._id)
+		})
+		.then(function (grid) {
+			// save, fb stuff
+			res.sendStatus(201);
+		})
+
+})
