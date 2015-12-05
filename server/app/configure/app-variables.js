8aeddce41e684c4a1e9306ea75fd0ac2cfc4b8ed
Jungho Son <js3692@users.noreply.github.com>
2015-12-04 19:10:22 -0500

    Started second turn

--- server/app/configure/app-variables.js
+++ server/app/configure/app-variables.js
@@ -10,18 +10,24 @@ var faviconPath = path.join(rootPath, './server/app/views/favicon.ico');
 var env = require(path.join(rootPath, './server/env'));
 
 var logMiddleware = function (req, res, next) {
-    util.log(('---NEW REQUEST---'));
-    console.log(util.format(chalk.red('%s: %s %s'), 'REQUEST ', req.method, req.path));
-    console.log(util.format(chalk.yellow('%s: %s'), 'QUERY   ', util.inspect(req.query)));
-    console.log(util.format(chalk.cyan('%s: %s'), 'BODY    ', util.inspect(req.body)));
-    console.log(util.format(chalk.green('%s: %s'), 'REQUEST IP    ', util.inspect(req.ip)));
-    next();
+  var message = chalk.blue(util.inspect(req.ip));
+  message += util.format(chalk.magenta(' %s %s'), req.method, req.path);
+  if (Object.keys(req.query).length) message += chalk.yellow(' QUERY RECEIVED\n') + chalk.cyan(util.inspect(req.query));
+  if (Object.keys(req.body).length) {
+    if(req.path.slice(0, 9) === '/api/play') message += chalk.cyan(' BODY RECEIVED\n') + chalk.yellow(util.inspect(req.body));
+    else {
+      if(Object.keys(req.body).length > 2) message += chalk.cyan(' BODY RECEIVED\n') + chalk.yellow(util.inspect(req.body));
+      else message += util.format(chalk.cyan(' %s: %s'), 'BODY', util.inspect(req.body));
+    }
+  } 
+  util.log(message);
+  next();
 };
 
 module.exports = function (app) {
-    app.setValue('env', env);
-    app.setValue('projectRoot', rootPath);
-    app.setValue('indexHTMLPath', indexPath);
-    app.setValue('faviconPath', faviconPath);
-    app.setValue('log', logMiddleware);
+  app.setValue('env', env);
+  app.setValue('projectRoot', rootPath);
+  app.setValue('indexHTMLPath', indexPath);
+  app.setValue('faviconPath', faviconPath);
+  app.setValue('log', logMiddleware);
 };
