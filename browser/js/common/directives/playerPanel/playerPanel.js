6a6fdb059b4b32fd8ef24ee14f059c0cc3bfa007
Daniel Peach <peach.daniel@gmail.com>
2015-12-06 13:05:16 -0500

    moved chat panel

--- browser/js/common/directives/playerPanel/playerPanel.js
+++ browser/js/common/directives/playerPanel/playerPanel.js
@@ -3,8 +3,12 @@ app.directive('playerPanel', function(SliderFactory, PlayGameFactory){
         restrict: 'E',
         templateUrl: 'js/common/directives/playerPanel/playerPanel.html',
         link: function(scope, elem, attrs){
+            
+            scope.view = 'players';
+            
             scope.getTurnOrder = PlayGameFactory.getTurnOrder;
             scope.open = SliderFactory.slideOut.bind(null, 'right');
+            scope.toggleArrows = scope.toggleArrows = SliderFactory.toggleSliderArrowsHandler('left');
         }
     };
 });
--- browser/js/common/factories/SliderFactory.js
+++ browser/js/common/factories/SliderFactory.js
@@ -6,11 +6,11 @@ app.factory('SliderFactory', function(){
             var sliders = [].slice.call(document.getElementsByClassName(side + '-slider'));
             sliders.forEach(slider => angular.element(slider).toggleClass(side + '-slider-pull'));
         },
-        toggleSliderArrowsHandler: function(){
+        toggleSliderArrowsHandler: function(startDirection){
             var triangleBase = 'glyphicon-triangle',
                 rightTriangle = '-right',
                 leftTriangle = '-left',
-                slider = angular.element([].slice.call(document.getElementsByClassName(triangleBase + rightTriangle)));
+                slider = angular.element([].slice.call(document.getElementsByClassName(triangleBase + '-' + startDirection)));
             return function() {
                 slider.toggleClass(triangleBase + rightTriangle);
                 slider.toggleClass(triangleBase + leftTriangle);
