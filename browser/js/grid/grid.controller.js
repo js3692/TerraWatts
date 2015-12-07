6fd5d38096462daf52f54ea061a2ff2ae69c01c5
Jungho Son <js3692@users.noreply.github.com>
2015-12-07 11:48:03 -0500

    Saving changes

--- browser/js/grid/grid.controller.js
+++ browser/js/grid/grid.controller.js
@@ -1,32 +1,36 @@
-app.controller('GridCtrl', function ($scope, $state, theUser, AppConstants, BeforeGameFactory, FirebaseFactory, gridId, key) {
-    $scope.grid = FirebaseFactory.getConnection(key);
+app.controller('GridCtrl', function ($scope, $state, BeforeGameFactory, FirebaseFactory, AppConstants, theUser, gridId, key) {
+  $scope.grid = FirebaseFactory.getConnection(key);
+  FirebaseFactory.getConnection(key).$loaded()
+    .then(function (grid) {
+      var emptySlots = grid.maxPlayers - grid.players.length;
+      $scope.players = grid.players.concat(_.fill(Array(emptySlots), null));
+    })
+  $scope.key = key;
+  $scope.me = theUser;
+
+  $scope.selected = theUser.color;
+  $scope.colors = ['purple', 'yellow', 'green', 'blue', 'red', 'black'];
+  $scope.changeColor = BeforeGameFactory.changeColor.bind(null, gridId, theUser._id)
+  $scope.colorPicked = function(color){
+    var players = $scope.grid.players;
+
+    if(!players) return false;
+
+    for(let i = 0, len = players.length; i < len; i++){
+      if(color === players[i].color) return .4;
+    }
+
+    return false;
+  };
+
     
-	$scope.me = theUser;
-    $scope.key = key;
-    
-    $scope.selected = theUser.color;
-    $scope.colors = ['purple', 'yellow', 'green', 'blue', 'red', 'black'];
-    $scope.changeColor = BeforeGameFactory.changeColor.bind(null, gridId, theUser._id)
-    $scope.colorPicked = function(color){
-        var players = $scope.grid.players;
-        if(!players) return false;
-        for(let i = 0, len = players.length; i < len; i++){
-            if(color === players[i].color) return .4;
-        }
-        return false;
-    };
-    
-    $scope.$watch('grid.game', function(game){
-        if(game) $state.go('game', { id: gridId, key: key });
-    });
+  $scope.$watch('grid.game', function(game){
+      if(game) $state.go('game', { id: gridId, key: key });
+  });
     
 	$scope.startGame = function() {
-		BeforeGameFactory
-            .start($scope.grid.id, $scope.grid.players);
-//            .then(function(updatedGrid){
-//                $state.go('game', { id: gridId, key: key });
-//            })
-	}; 
+		BeforeGameFactory.start($scope.grid.id, $scope.grid.players);
+	};
 
 	$scope.leaveGame = function() {
     angular.element("#grid")
@@ -37,6 +41,6 @@ app.controller('GridCtrl', function ($scope, $state, theUser, AppConstants, Befo
           .then(function() {
             $state.go('home');
           });
-      })
+      });
 	};
-})
+});
